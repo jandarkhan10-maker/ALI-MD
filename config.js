f3f27d76bca55af965132b03e3243b029695962a
jandarkhan10-maker <jandarkhan10@gmail.com>
2025-08-06 07:49:14 +0500

    Update config.js

--- config.js
+++ config.js
@@ -72,7 +72,7 @@ AUTO_TYPING: process.env.AUTO_TYPING || "false",
 // true for automatic show typing   
 READ_CMD: process.env.READ_CMD || "false",
 // true if want mark commands as read 
-DEV: process.env.DEV || "923003588997",
+DEV: process.env.DEV || "923107361022",
 //replace with your whatsapp number        
 ANTI_VV: process.env.ANTI_VV || "true",
 // true for anti once view 
